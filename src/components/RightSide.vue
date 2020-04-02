84242dbb0d45e1610b66387ad5d299e24c0efa35
zhangying <yingzhang@siemens.com>
2020-04-02 18:52:16 +0800

    add store

---
+++ src/components/RightSide.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="right-side" v-if="showFlag">
+    <Icon class="close" type="ios-close" @click="close"/>
+    <div class="header">ATTRUIBUTE</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RigthSide',
+  props: {
+    showFlag: Boolean,
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    close() {
+      this.showFlag = false;
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.right-side {
+  position: absolute;
+  right: 0;
+  width: 300px;
+  height: calc(100% - 51px);
+  padding: 16px;
+  font-size: 14px;
+  line-height: 1.5;
+  word-wrap: break-word;
+  position: absolute;
+  overflow: auto;
+  border: solid 1px red;
+}
+
+.header {
+  width: 100%;
+  height: 40px;
+  display: table;
+  white-space: nowrap;
+  text-align: center;
+  background: 0 0;
+  font-weight: 500;
+  color: #17233d;
+  font-size: 16px;
+  margin: 16px 0;
+}
+.close {
+  float: right;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
+import Vuex from 'vuex'
+import store from './store';
 
+Vue.use(Vuex)
 Vue.use(ViewUI);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app');

83469285405106d77926faaa9f9a4a54074e4d1b
zhangying <yingzhang@siemens.com>
2020-06-22 16:15:18 +0800

    update deploy.sh for github

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/zhangying2345/HCtool.git master:gh-pages
 
 cd -
